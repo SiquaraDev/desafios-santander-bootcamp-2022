050b231ee2edfa11ef0a463d9370661e0ed70e2d
Matheus Mendonça <matheus.siquara99@gmail.com>
2022-06-18 15:54:11 -0300

    módulos js

---
+++ Javascript/Trabalhando com Módulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraCidade('Matheus', 'Rio de Janeiro'));
+console.log(mostraIdade('Matheus', 21));
+console.log(mostraHobby('Matheus', 'jogar com amigos'));
