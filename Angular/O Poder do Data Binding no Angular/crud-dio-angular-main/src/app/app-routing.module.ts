cd578240c851c26ec32cb0a3b23c180987599978
Matheus Mendonça <matheus.siquara99@gmail.com>
2022-06-30 15:51:55 -0300

    data binding

---
+++ Angular/O Poder do Data Binding no Angular/crud-dio-angular-main/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { DataBindingComponent } from './pages/data-binding/data-binding.component';
+import { UserFormComponent } from './pages/users/user-form/user-form.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { UsersListComponent } from './pages/users/users-list/users-list.component';
+
+const routes: Routes = [
+  { path: '', component: UsersListComponent},
+  { path: 'form', component: UserFormComponent},
+  { path: 'form/:id', component: UserFormComponent},
+  { path: 'data-binding', component: DataBindingComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
